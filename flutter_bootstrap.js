8a5e9b1b9f5887cfa6b87e56aa3697710fb249b4
Dolful <github-bot@waveful.app>
2025-07-23 13:00:41 +0000

    Update website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "353939541"
+    serviceWorkerVersion: "3060152793"
   }
 });
